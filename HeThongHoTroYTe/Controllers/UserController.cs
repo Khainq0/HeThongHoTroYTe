6dce3f2c35d11524fa5203763fbb747b4531444a
Khainq0 <n2k29102000@gmail.com>
2021-04-05 20:45:06 +0700

    Add project files.

---
+++ HeThongHoTroYTe/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HeThongHoTroYTe.Models;
+
+namespace HeThongHoTroYTe.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var x = Session["username"];
+            if (x == null)
+            {
+                return View("Login");
+            }
+            else
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginAction(User us)
+        {
+            if ("admin".Equals(us.username) && "123456".Equals(us.pwd))
+            {
+                Session["username"] = us.username;
+                return RedirectToAction("Index", "User");
+
+
+            }
+            else
+            {
+
+                return RedirectToAction("Login", "User");
+            }
+        }
+        
+            }
+}
